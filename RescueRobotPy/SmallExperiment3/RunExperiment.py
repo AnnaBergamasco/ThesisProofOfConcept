3810fdb34d3fc341aa0e77f0836ea7057ab36925
AnnaBergamasco <anna.bergamasco@mail.polimi.it>
2022-11-10 11:03:35 +0100

    Result files production code added to experiment 3

--- RescueRobotPy/SmallExperiment3/RunExperiment.py
+++ RescueRobotPy/SmallExperiment3/RunExperiment.py
@@ -6,6 +6,51 @@ from RadarPlotAlgorithms import RadarPlotAlgorithms
 from RadarPlotSummary import RadarPlotSummary
 from BoxPlot import BoxPlotVariables
 
+def makeResultFile(alg_name, results) -> None:
+
+    
+    file = open(alg_name + '_results.txt', 'w')
+    
+    file.write('[MAXIMUM SEVERITY]\n')
+    file.write('\t[LOWER BOUND]\n')
+    file.write('\t\t[S0 e1 S1]' + str(results[0][0]) + '\n')
+    file.write('\t\t[S0 e1 S2]' + str(results[0][1]) + '\n')
+    file.write('\t\t[S0 e1 S6]' + str(results[0][2]) + '\n')
+    file.write('\t\t[S3 e1 S1]' + str(results[0][3]) + '\n')
+    file.write('\t\t[S3 e1 S4]' + str(results[0][4]) + '\n')
+    file.write('\t\t[S3 e1 S5]' + str(results[0][5]) + '\n')
+    file.write('\t[HIGH BOUND]\n')
+    file.write('\t\t[S0 e1 S1]' + str(results[1][0]) + '\n')
+    file.write('\t\t[S0 e1 S2]' + str(results[1][1]) + '\n')
+    file.write('\t\t[S0 e1 S6]' + str(results[1][2]) + '\n')
+    file.write('\t\t[S3 e1 S1]' + str(results[1][3]) + '\n')
+    file.write('\t\t[S3 e1 S4]' + str(results[1][4]) + '\n')
+    file.write('\t\t[S3 e1 S5]' + str(results[1][5]) + '\n')
+    file.write('[AVERAGE SEVERITY]\n')
+    file.write('\t[LOWER BOUND]\n')
+    file.write('\t\t[S0 e1 S1]' + str(results[2][0]) + '\n')
+    file.write('\t\t[S0 e1 S2]' + str(results[2][1]) + '\n')
+    file.write('\t\t[S0 e1 S6]' + str(results[2][2]) + '\n')
+    file.write('\t\t[S3 e1 S1]' + str(results[2][3]) + '\n')
+    file.write('\t\t[S3 e1 S4]' + str(results[2][4]) + '\n')
+    file.write('\t\t[S3 e1 S5]' + str(results[2][5]) + '\n')
+    file.write('\t[HIGH BOUND]\n')
+    file.write('\t\t[S0 e1 S1]' + str(results[3][0]) + '\n')
+    file.write('\t\t[S0 e1 S2]' + str(results[3][1]) + '\n')
+    file.write('\t\t[S0 e1 S6]' + str(results[3][2]) + '\n')
+    file.write('\t\t[S3 e1 S1]' + str(results[3][3]) + '\n')
+    file.write('\t\t[S3 e1 S4]' + str(results[3][4]) + '\n')
+    file.write('\t\t[S3 e1 S5]' + str(results[3][5]) + '\n')
+    file.write('\t[AVERAGE]\n')
+    file.write('\t\t[S0 e1 S1]' + str(results[4][0]) + '\n')
+    file.write('\t\t[S0 e1 S2]' + str(results[4][1]) + '\n')
+    file.write('\t\t[S0 e1 S6]' + str(results[4][2]) + '\n')
+    file.write('\t\t[S3 e1 S1]' + str(results[4][3]) + '\n')
+    file.write('\t\t[S3 e1 S4]' + str(results[4][4]) + '\n')
+    file.write('\t\t[S3 e1 S5]' + str(results[4][5]) + '\n')
+
+    file.close()
+
 def summarizeData(alg_severities) -> list:
 
     summary = [[], [], [], [], []]
@@ -62,21 +107,27 @@ def main():
     nsga3_summary = summarizeData(nsga3_scaled)
     plot_algorithms = RadarPlotAlgorithms(nsga3_summary[0], nsga3_summary[1], nsga3_summary[2], nsga3_summary[3])
     plot_algorithms.makePlot()
+    makeResultFile('nsga3', nsga3_summary)
     unsga3_summary = summarizeData(unsga3_scaled)
     plot_algorithms = RadarPlotAlgorithms(unsga3_summary[0], unsga3_summary[1], unsga3_summary[2], unsga3_summary[3])
     plot_algorithms.makePlot()
+    makeResultFile('unsga3', unsga3_summary)
     agemoea_summary = summarizeData(agemoea_scaled)
     plot_algorithms = RadarPlotAlgorithms(agemoea_summary[0], agemoea_summary[1], agemoea_summary[2], agemoea_summary[3])
     plot_algorithms.makePlot()
+    makeResultFile('agemoea', agemoea_summary)
     ctaea_summary = summarizeData(ctaea_scaled)
     plot_algorithms = RadarPlotAlgorithms(ctaea_summary[0], ctaea_summary[1], ctaea_summary[2], ctaea_summary[3])
     plot_algorithms.makePlot()
+    makeResultFile('ctaea', ctaea_summary)
     moead_summary = summarizeData(moead_scaled)
     plot_algorithms = RadarPlotAlgorithms(moead_summary[0], moead_summary[1], moead_summary[2], moead_summary[3])
     plot_algorithms.makePlot()
+    makeResultFile('moead', moead_summary)
     random_summary = summarizeData(random_scaled)
     plot_algorithms = RadarPlotAlgorithms(random_summary[0], random_summary[1], random_summary[2], random_summary[3])
     plot_algorithms.makePlot()
+    makeResultFile('random', random_summary)
 
     plot_summary = RadarPlotSummary([nsga3_summary[1],
             unsga3_summary[1],
